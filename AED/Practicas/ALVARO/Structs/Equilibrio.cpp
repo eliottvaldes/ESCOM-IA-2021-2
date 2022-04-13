13b5add2a88102ff0371a0c66e52c8bbe0cbf79e
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-04-13 17:05:51 -0500

    add_biblioteca e inicio

---
+++ AED/Practicas/ALVARO/Structs/Equilibrio.cpp
@@ -0,0 +1,19 @@
+/*
+======================================
+ ▪* Montero Barraza Alvaro David*
+     *2BV1 Ingenieria en IA ▪*  
+=======================================
+ */
+#include<iostream>
+#include<stdlib.h>
+#include<equilibrio.h>
+using namespace std;
+
+int main(){
+    string cadena;
+    cout<<"Introduce un string para ser analizado";
+    cin>>cadena;
+
+
+
+}
---
+++ AED/Practicas/ALVARO/Structs/equilibrio.h
@@ -0,0 +1,17 @@
+/*
+======================================
+ ▪* Montero Barraza Alvaro David*
+     *2BV1 Ingenieria en IA ▪*  
+=======================================
+ */
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+string cola_push(){
+
+}
+string cola_pop(){
+
+    
+}
