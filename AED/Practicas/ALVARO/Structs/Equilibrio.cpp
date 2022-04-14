3b84360473bdd0012125270f8852d0a8fbf93e77
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-04-13 22:54:18 -0500

    mod-equilibrio con biblioteca stl

--- AED/Practicas/ALVARO/Structs/Equilibrio.cpp
+++ AED/Practicas/ALVARO/Structs/Equilibrio.cpp
@@ -5,14 +5,65 @@
 =======================================
  */
 #include<iostream>
+#include<string>
+#include<stack>
 #include<stdlib.h>
-#include<equilibrio.h>
+#include<C:\Users\Montero\Desktop\Algoritmos\Equilibrio\equilibrio.h>
 using namespace std;
 
 int main(){
+    stack<char> signo;
     string cadena;
-    cout<<"Introduce un string para ser analizado";
+    bool flag=true;
+    int tam;
+    cout<<"Introduce un string para ser analizado"<<endl;
     cin>>cadena;
+    tam=cadena.size();
+    cout<<tam<<endl;
+    for (int i = 0; i < tam; i++)
+    {
+        switch (cadena[i])
+        {
+        case '(':
+            signo.push('(');
+            break;
+        case '[':
+            signo.push('[');
+            break;
+        case ')':
+            if (signo.top()=='(')
+            {
+                signo.pop();
+                flag=true;
+            }else{
+                flag=false;
+            }
+            
+            break;
+        case ']':
+            if(signo.top()=='['){
+                signo.pop();
+                flag=true;
+            }else{
+                flag=false;
+            }
+            break;
+        
+        default:
+            break;
+        }
+        
+    }
+    if(signo.size()>0){
+        flag=false;
+    }
+    if(flag){
+        cout<<"Existe equilibrio"<<endl;
+    }else{
+        cout<<"No existe equilibrio"<<endl;
+    }
+    system("pause");
+    
 
 
 
