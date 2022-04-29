7609d3f84137dd9261d18fbc37dcd47b1734f9eb
Jose <mnry7999@gmail.com>
2022-04-29 12:31:16 -0500

    add: Push, pop and top funtions

--- AED/Practicas/MONROY/equilibrioLista.h
+++ AED/Practicas/MONROY/equilibrioLista.h
@@ -12,10 +12,35 @@ struct Nodo
     char caracter;
     Nodo *sig;
 };
+void push_Nodo(Nodo *nuevo, char caracter)
+{
+    nuevo->caracter = caracter;
+    nuevo->sig = NULL;
+}
+void lista_push(Nodo **lista, char caracter)
+{
+    Nodo *nuevo = new Nodo;
+    push_Nodo(nuevo, caracter);
+    if (*lista == NULL)
+    {
+        *lista = nuevo;
+    }
+    else
+    {
+        nuevo->sig = *lista;
+        *lista = nuevo;
+    }
+}
 
-void lista_push( Nodo **, char );
-
-void lista_pop(Nodo **);
-void lista_top(Nodo *);
+void lista_pop(Nodo **lista)
+{
+    Nodo *aux = *lista;
+    *lista = (*lista)->sig;
+    delete aux;
+}
+int lista_top(Nodo *lista)
+{
+    return lista->caracter;
+}
 void lista_empty(Nodo *);
 void lista_size(Nodo *);
