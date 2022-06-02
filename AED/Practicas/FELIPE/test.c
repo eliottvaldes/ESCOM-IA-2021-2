a180c530fa7df3ff2b14e50267debbed93e5a913
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-06-02 18:55:45 -0500

    BST

--- AED/Practicas/FELIPE/test.c
+++ AED/Practicas/FELIPE/test.c
@@ -1,20 +1,62 @@
-#include "TDAPract1_Philip.h"
-int main(){
-    int valor1=0,valor2=0;
-    puts("Cree su TDA con los valores 1 y 2 de tipo entero");
-    puts("Ingrese el valor 1");
-    scanf("%d", &valor1);
-    puts("Ingrese el valor 2");
-    scanf("%d", &valor2);
-    TDA TDACreado;
-    CrearTDA(valor1,valor2, &TDACreado);
-    ImprimirTDA(TDACreado);
-    TDA TDACopia;
-    CopiarTDA(TDACreado, &TDACopia);
-    ImprimirTDA(TDACopia);
-    EliminarTDA(&TDACreado);
-    //ImprimirTDA(TDACreado);
-    return 0;
+#include "arbolBST.h"
+
+void iniciaOperacion(int argc, char** argv){
+	//Se creara un arbol con el siguiente orden de datos: 1,7,8,10,15,1,13,5,7,25,689
+	
+	//Se crea un arbol vacío
+	Arbol raiz=NULL;
+	Dato valorUser=1;
+	puts("Iniciando ingreso de datos");
+	ingresarNodo(&raiz, valorUser);
+	valorUser=7;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=8;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=10;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=15;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=4;
+	ingresarNodo(&raiz, valorUser);	
+	valorUser=13;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=5;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=7;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=25;
+	ingresarNodo(&raiz, valorUser);
+	valorUser=689;
+	ingresarNodo(&raiz, valorUser);
+	puts("Imprimiendo in orden");
+	//Se confirma el ordenamiento natural del arbol
+	inOrden(raiz);
+	puts("\nImpresión pos-Orden:");
+	posOrden(raiz);
+	puts("\nImpresión pre-orden");
+	preOrden(raiz);
+	
+	//Buscando un nodo 7:
+	Arbol nodoEncontrado=NULL;
+	buscarNodo(raiz,7, &nodoEncontrado);
+	if(!nodoEncontrado)
+		puts("Nodo no encontrado");
+	else
+		printf("\nSe encontro el nodo con el valor %d\n", nodoEncontrado->info);
+	
+	//Eliminando nodo 13
+	raiz=deleteNodeBST(raiz,7);
+	puts("\nImprimiendo in orden sin el nodo eliminado");
+	//Se valida la eliminación
+	inOrden(raiz);
+	
+	//Eliminando arbol
+	eliminarArbol(&raiz);
+	puts("\nSe elimina arbol");
+	puts("\nImpresión pos-Orden:");
+	posOrden(raiz);
 }
 
-//Compilación:  gcc miLibreria.h miLibreria.c test.c -O3 -o programa
+void main(int argc, char** argv){
+	iniciaOperacion(argc, argv);
+}
