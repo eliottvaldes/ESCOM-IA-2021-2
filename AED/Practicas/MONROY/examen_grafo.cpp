1319ce9874e83f095cdc06764763d6d768b065fc
Jose Monroy <mnry7999@gmail.com>
2022-06-12 18:04:07 -0500

    Conversión y filtración de datos obtenidos

--- AED/Practicas/MONROY/examen_grafo.cpp
+++ AED/Practicas/MONROY/examen_grafo.cpp
@@ -12,8 +12,12 @@ string obtenerDatos();
 int obtenerDatosNum();
 
 int main(){
+<<<<<<< HEAD
     string nombresNodos, vinculos;
     int noNodos, noAristas, costo;
     nombresNodos, vinculos = obtenerDatos();
     noNodos, noAristas = obtenerDatosNum();
+=======
+    
+>>>>>>> 2c6b1c8 (Conversión y filtración de datos obtenidos)
 }
