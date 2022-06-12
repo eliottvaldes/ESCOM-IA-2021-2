22288c6f519643d4b9c57311bde4e3d64e766a33
Jose Monroy <mnry7999@gmail.com>
2022-06-12 16:51:36 -0500

    Declaraciones iniciales

--- AED/Practicas/MONROY/examen_grafo.cpp
+++ AED/Practicas/MONROY/examen_grafo.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+struct grafo{
+    int V;
+    int E;
+    int **matriz;
+};
+
+string obtenerDatos();
+int obtenerDatosNum();
+
+int main(){
+    string nombresNodos, vinculos;
+    int noNodos, noAristas, costo;
+    nombresNodos, vinculos = obtenerDatos();
+    noNodos, noAristas = obtenerDatosNum();
+}
