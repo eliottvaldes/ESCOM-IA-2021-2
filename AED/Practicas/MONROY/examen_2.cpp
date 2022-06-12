af0598ed1d6d9dd9b78d9d4d523a30b9bec2c805
Jose Monroy <mnry7999@gmail.com>
2022-06-12 15:53:30 -0500

    Declaraciones iniciales

---
+++ AED/Practicas/MONROY/examen_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+struct nodo {
+    int dato;
+    nodo *derecha;
+    nodo *izquierda;
+};
+
+void agregarNodo(nodo*&, int);
+nodo* nuevoNodo(int);
+
+int main(){
+    
+}
